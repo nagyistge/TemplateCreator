3d8b809fa46b59e572c33bed6769e46e603e1736
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-08-04 01:19:10 +0200

    Added circle and rect tools

--- TemplateCreator/MainWindow.xaml.cs
+++ TemplateCreator/MainWindow.xaml.cs
@@ -19,6 +19,25 @@ using System.Windows.Shapes;
 
 namespace TemplateCreator
 {
+    #region Tool
+
+    public enum Tool
+    {
+        None,
+        Line,
+        Polyline,
+        Path,
+        Rect,
+        Circle,
+        Text,
+        Move,
+        Scale,
+        Rotate,
+        Duplicate
+    }
+
+    #endregion
+
     #region LineGuidesAdorner
 
     public class LineGuidesAdorner : Adorner
@@ -113,6 +132,17 @@ namespace TemplateCreator
                 new FrameworkPropertyMetadata(0.0,
                     FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
 
+        public Tool Tool
+        {
+            get { return (Tool)GetValue(ToolProperty); }
+            set { SetValue(ToolProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToolProperty =
+            DependencyProperty.Register("Tool", typeof(Tool), typeof(LineGuidesAdorner),
+                new FrameworkPropertyMetadata(Tool.None,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
         #endregion
 
         #region Constructor
@@ -124,7 +154,16 @@ namespace TemplateCreator
 
         #endregion
 
-        #region Pens
+        #region Pens & Brushes
+
+        Brush BrushRect = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+
+        Pen PenRect = new Pen(new SolidColorBrush(Color.FromArgb(255, 0, 0, 255)), 0.0)
+        {
+            StartLineCap = PenLineCap.Flat,
+            EndLineCap = PenLineCap.Flat,
+            LineJoin = PenLineJoin.Miter
+        };
 
         Pen PenGuides = new Pen(new SolidColorBrush(Color.FromArgb(255, 0, 0, 255)), 1.0)
         {
@@ -169,9 +208,36 @@ namespace TemplateCreator
                     drawingContext.DrawLine(PenGuides, horizontalPoint0, horizontalPoint1);
                 }
 
-                var p0 = new Point(X1, Y1);
-                var p1 = new Point(X2, Y2);
-                drawingContext.DrawLine(PenElement, p0, p1);
+                if (Tool == Tool.Line || Tool == Tool.Polyline)
+                {
+                    var p0 = new Point(X1, Y1);
+                    var p1 = new Point(X2, Y2);
+                    drawingContext.DrawLine(PenElement, p0, p1);
+                }
+                else if (Tool == Tool.Rect)
+                {
+                    var p0 = new Point(X1, Y1);
+                    var p1 = new Point(X2, Y2);
+                    var rect = new Rect(p0, p1);
+
+                    drawingContext.DrawRectangle(BrushRect, PenRect, rect);
+                }
+                else if (Tool == Tool.Circle)
+                {
+                    var p0 = new Point(X1, Y1);
+                    var p1 = new Point(X2, Y2);
+                    var rect = new Rect(p0, p1);
+
+                    double radiusX = (rect.Right - rect.Left) / 2.0;
+                    double radiusY = (rect.Bottom - rect.Top) / 2.0;
+
+                    double centerX = rect.Left + radiusX;
+                    double centerY = rect.Top + radiusY;
+
+                    var center = new Point(centerX, centerY);
+
+                    drawingContext.DrawEllipse(BrushRect, PenRect, center, radiusX, radiusY);
+                }
             }
         }
 
@@ -190,8 +256,8 @@ namespace TemplateCreator
 
         private bool snapWhenCreating = true;
         private bool snapWhenMoving = true;
-        private bool polyLineMode = false;
-        private bool scaleMode = false;
+
+        private Tool tool = Tool.Line;
 
         private Line OriginaLine = null;
 
@@ -321,10 +387,66 @@ namespace TemplateCreator
             {
                 switch (key)
                 {
+                    // line
+                    case Key.L:
+                        {
+                            tool = Tool.Line;
+                        }
+                        break;
+
+                    // polyline
+                    case Key.P:
+                        {
+                            tool = Tool.Polyline;
+                        }
+                        break;
+
+                    // rect
+                    case Key.R:
+                        {
+                            tool = Tool.Rect;
+                        }
+                        break;
+
+                    // circle
+                    case Key.C:
+                        {
+                            tool = Tool.Circle;
+                        }
+                        break;
+
+                    // text
+                    case Key.T:
+                        {
+                            tool = Tool.Text;
+                        }
+                        break;
+
+                    // move
+                    case Key.V:
+                        {
+                            tool = Tool.Move;
+                        }
+                        break;
+
                     // scale
                     case Key.S:
                         {
-                            scaleMode = !scaleMode;
+                            tool = Tool.Scale;
+                        }
+                        break;
+
+                    // rotate
+                    case Key.E:
+                        {
+                            tool = Tool.Rotate;
+                        }
+                        break;
+
+                    // duplicate
+                    case Key.D:
+                        {
+                            tool = Tool.Duplicate;
                         }
                         break;
 
@@ -332,20 +454,6 @@ namespace TemplateCreator
                     case Key.M:
                         {
                             snapWhenCreating = !snapWhenCreating;
-                        }
-                        break;
-
-                    // line
-                    case Key.L:
-                        {
-                            polyLineMode = false;
-                        }
-                        break;
-
-                    // polyline
-                    case Key.P:
-                        {
-                            polyLineMode = true;
                         }
                         break;
                 }
@@ -428,7 +536,7 @@ namespace TemplateCreator
         {
             if (canvas.IsMouseCaptured)
             {
-                if (OriginaLine != null || polyLineMode == false)
+                if (OriginaLine != null || tool != Tool.Polyline)
                     canvas.ReleaseMouseCapture();
 
                 var p = e.GetPosition(canvas);
@@ -438,13 +546,34 @@ namespace TemplateCreator
                 double x = snapWhenCreating == true ? Snap(p.X, snapX, snapOffsetX) : p.X;
                 double y = snapWhenCreating == true ? Snap(p.Y, snapY, snapOffsetY) : p.Y;
 
-                if (((adorner.X1 == adorner.X2) && (adorner.Y1 == adorner.Y2)) == false)
+                if (tool == Tool.Line || 
+                    tool == Tool.Polyline ||
+                    (tool == Tool.Scale && OriginaLine != null))
                 {
-                    var l = CreateLine(adorner.X1, adorner.Y1, x, y);
-                    canvas.Children.Add(l);
+                    if (((adorner.X1 == adorner.X2) && (adorner.Y1 == adorner.Y2)) == false)
+                    {
+                        var line = CreateLine(adorner.X1, adorner.Y1, x, y);
+                        canvas.Children.Add(line);
+                    }
+                }
+                else if (tool == Tool.Rect)
+                {
+                    if (((adorner.X1 == adorner.X2) && (adorner.Y1 == adorner.Y2)) == false)
+                    {
+                        var rect = CreateRect(adorner.X1, adorner.Y1, x, y);
+                        canvas.Children.Add(rect);
+                    }
+                }
+                else if (tool == Tool.Circle)
+                {
+                    if (((adorner.X1 == adorner.X2) && (adorner.Y1 == adorner.Y2)) == false)
+                    {
+                        var circle = CreateCircle(adorner.X1, adorner.Y1, x, y);
+                        canvas.Children.Add(circle);
+                    }
                 }
 
-                if (OriginaLine == null && polyLineMode == true)
+                if (OriginaLine == null && tool == Tool.Polyline)
                 {
                     // create next line
                     adorner.X = x;
@@ -467,7 +596,7 @@ namespace TemplateCreator
             {
                 System.Diagnostics.Debug.Print("{0}", e.OriginalSource.GetType());
 
-                if (scaleMode == true)
+                if (tool == Tool.Scale)
                 {
                     if (e.OriginalSource is Line)
                     {
@@ -495,12 +624,12 @@ namespace TemplateCreator
 
                         if (d1 > d2)
                         {
-                            AddAdorner(x1, y1, x, x);
+                            AddAdorner(x1, y1, x, x, Tool.Line);
                             UpdateTitle(x1, y1, x, x);
                         }
                         else
                         {
-                            AddAdorner(x2, y2, x, x);
+                            AddAdorner(x2, y2, x, x, Tool.Line);
                             UpdateTitle(x2, y2, x, x);
                         }
 
@@ -513,7 +642,7 @@ namespace TemplateCreator
                     double x = snapWhenCreating == true ? Snap(p.X, snapX, snapOffsetX) : p.X;
                     double y = snapWhenCreating == true ? Snap(p.Y, snapY, snapOffsetY) : p.Y;
 
-                    AddAdorner(x, y, x, y);
+                    AddAdorner(x, y, x, y, tool);
 
                     UpdateTitle(x, y, x, y);
 
@@ -526,13 +655,14 @@ namespace TemplateCreator
 
         #region Adorner
 
-        private void AddAdorner(double x1, double y1, double x2, double y2)
+        private void AddAdorner(double x1, double y1, double x2, double y2, Tool tool)
         {
             var adornerLayer = AdornerLayer.GetAdornerLayer(canvas);
             adorner = new LineGuidesAdorner(canvas);
 
             RenderOptions.SetEdgeMode(adorner, EdgeMode.Aliased);
 
+            adorner.Tool = tool;
             adorner.CanvasWidth = canvas.Width;
             adorner.CanvasHeight = canvas.Height;
             adorner.X = x2;
@@ -544,8 +674,9 @@ namespace TemplateCreator
 
             adornerLayer.Add(adorner);
 
-            var setZOrderMethodInfo = adornerLayer.GetType().GetMethod("SetAdornerZOrder", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            setZOrderMethodInfo.Invoke(adornerLayer, new object[] { adorner, 10 });
+            //var setZOrderMethodInfo = adornerLayer.GetType().GetMethod("SetAdornerZOrder", 
+            //    System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            //setZOrderMethodInfo.Invoke(adornerLayer, new object[] { adorner, 5 });
         }
 
         private void RemoveAdorner()
@@ -573,47 +704,97 @@ namespace TemplateCreator
 
         #region Line
 
-        private void SetLineStart(Line line, double x, double y, bool snap)
+        private Line CreateLine(double x1, double y1, double x2, double y2)
         {
-            line.X1 = snap == true ? Snap(x, snapX, snapOffsetX) : x;
-            line.Y1 = snap == true ? Snap(y, snapY, snapOffsetY) : y;
-        }
-
-        private void SetLineEnd(Line line, double x, double y, bool snap)
-        {
-            line.X2 = snap == true ? Snap(x, snapX, snapOffsetX) : x;
-            line.Y2 = snap == true ? Snap(y, snapY, snapOffsetY) : y;
-        }
-
-        private Line CreateLine()
-        {
-            var l = new Line()
+            var line = new Line()
             {
                 Stroke = Brushes.Red,
                 StrokeThickness = 5.0,
                 StrokeLineJoin = PenLineJoin.Miter,
                 StrokeStartLineCap = PenLineCap.Square,
                 StrokeEndLineCap = PenLineCap.Square,
+                Fill = Brushes.Transparent,
+                SnapsToDevicePixels = false
+            };
+
+            Panel.SetZIndex(line, 3);
+
+            RenderOptions.SetEdgeMode(line, EdgeMode.Aliased);
+
+            line.X1 = x1;
+            line.Y1 = y1;
+            line.X2 = x2;
+            line.Y2 = y2;
+
+            return line;
+        }
+
+        #endregion
+
+        #region Rect
+
+        private Rectangle CreateRect(double x1, double y1, double x2, double y2)
+        {
+            var rect = new Rectangle()
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 0.0,
+                StrokeLineJoin = PenLineJoin.Miter,
+                StrokeStartLineCap = PenLineCap.Square,
+                StrokeEndLineCap = PenLineCap.Square,
+                Fill = Brushes.Red,
                 SnapsToDevicePixels = false,
             };
 
-            Panel.SetZIndex(l, 3);
+            Panel.SetZIndex(rect, 3);
 
-            RenderOptions.SetEdgeMode(l, EdgeMode.Aliased);
+            RenderOptions.SetEdgeMode(rect, EdgeMode.Aliased);
 
-            return l;
+            var p0 = new Point(x1, y1);
+            var p1 = new Point(x2, y2);
+            var r = new Rect(p0, p1);
+
+            rect.Width = r.Width;
+            rect.Height = r.Height;
+
+            Canvas.SetLeft(rect, r.Left);
+            Canvas.SetTop(rect, r.Top);
+
+            return rect;
         }
 
-        private Line CreateLine(double x1, double y1, double x2, double y2)
+        #endregion
+
+        #region Circle
+
+        private Ellipse CreateCircle(double x1, double y1, double x2, double y2)
         {
-            var l = CreateLine();
+            var ellipse = new Ellipse()
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 0.0,
+                StrokeLineJoin = PenLineJoin.Miter,
+                StrokeStartLineCap = PenLineCap.Square,
+                StrokeEndLineCap = PenLineCap.Square,
+                Fill = Brushes.Red,
+                SnapsToDevicePixels = false,
+            };
 
-            l.X1 = x1;
-            l.Y1 = y1;
-            l.X2 = x2;
-            l.Y2 = y2;
+            Panel.SetZIndex(ellipse, 3);
 
-            return l;
+            RenderOptions.SetEdgeMode(ellipse, EdgeMode.Aliased);
+
+            var p0 = new Point(x1, y1);
+            var p1 = new Point(x2, y2);
+            var r = new Rect(p0, p1);
+
+            ellipse.Width = r.Width;
+            ellipse.Height = r.Height;
+
+            Canvas.SetLeft(ellipse, r.Left);
+            Canvas.SetTop(ellipse, r.Top);
+
+            return ellipse;
         }
 
         #endregion
@@ -627,24 +808,64 @@ namespace TemplateCreator
 
         private string GenerateModel()
         {
-            var lines = canvas.Children.OfType<Line>();
+            var children = canvas.Children;
             var sb = new StringBuilder();
 
-            foreach (var line in lines)
+            foreach (var child in children)
             {
-                double x1 = line.X1;
-                double y1 = line.Y1;
-                double x2 = line.X2;
-                double y2 = line.Y2;
+                if (child is Line)
+                {
+                    var line = child as Line;
 
-                sb.AppendFormat("{0};{1};{2};{3};{4}{5}",
-                    "line",
-                    DoubleToString(x1),
-                    DoubleToString(y1),
-                    DoubleToString(x2),
-                    DoubleToString(y2),
-                    Environment.NewLine);
+                    double x1 = line.X1;
+                    double y1 = line.Y1;
+                    double x2 = line.X2;
+                    double y2 = line.Y2;
+
+                    sb.AppendFormat("{0};{1};{2};{3};{4}{5}",
+                        "line",
+                        DoubleToString(x1),
+                        DoubleToString(y1),
+                        DoubleToString(x2),
+                        DoubleToString(y2),
+                        Environment.NewLine);
+                }
+                else if (child is Rectangle)
+                {
+                    var rect = child as Rectangle;
+
+                    double x1 = Canvas.GetLeft(rect);
+                    double y1 = Canvas.GetTop(rect);
+                    double x2 = x1 + rect.Width;
+                    double y2 = y1 + rect.Height;
+
+                    sb.AppendFormat("{0};{1};{2};{3};{4}{5}",
+                        "rect",
+                        DoubleToString(x1),
+                        DoubleToString(y1),
+                        DoubleToString(x2),
+                        DoubleToString(y2),
+                        Environment.NewLine);
+                }
+                else if (child is Ellipse)
+                {
+                    var circle = child as Ellipse;
+
+                    double x1 = Canvas.GetLeft(circle);
+                    double y1 = Canvas.GetTop(circle);
+                    double x2 = x1 + circle.Width;
+                    double y2 = y1 + circle.Height;
+                    
+                    sb.AppendFormat("{0};{1};{2};{3};{4}{5}",
+                        "circle",
+                        DoubleToString(x1),
+                        DoubleToString(y1),
+                        DoubleToString(x2),
+                        DoubleToString(y2),
+                        Environment.NewLine);
+                }
             }
+
 
             return sb.ToString();
         }
@@ -667,6 +888,28 @@ namespace TemplateCreator
 
                     var l = CreateLine(x1, y1, x2, y2);
                     canvas.Children.Add(l);
+                }
+                else if (args.Length == 5 &&
+                    string.Compare(args[0], "rect", StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    double x1 = double.Parse(args[1]);
+                    double y1 = double.Parse(args[2]);
+                    double x2 = double.Parse(args[3]);
+                    double y2 = double.Parse(args[4]);
+
+                    var r = CreateRect(x1, y1, x2, y2);
+                    canvas.Children.Add(r);
+                }
+                else if (args.Length == 5 &&
+                    string.Compare(args[0], "circle", StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    double x1 = double.Parse(args[1]);
+                    double y1 = double.Parse(args[2]);
+                    double x2 = double.Parse(args[3]);
+                    double y2 = double.Parse(args[4]);
+
+                    var c = CreateCircle(x1, y1, x2, y2);
+                    canvas.Children.Add(c);
                 }
             }
         }
